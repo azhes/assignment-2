e8e3fa9e46206b140c05f2a888b61a5c4204d48f
Azhes <graystonea@gmail.com>
2021-03-29 11:57:37 -0700

    list functions needed for assignment 2

---
+++ JS/app.js
@@ -0,0 +1,13 @@
+/* Function to make dark mode button work */
+
+/* Function to make the new note button work */
+
+/* Function to make the save button work */
+
+/* Function to store notes in a notesArray */
+
+/* Function to display note title and list of notes in sidebar */
+
+/* Function to make the delete or cancel button work */
+
+/* Function to display note when title in sidebar is clicked on */
