7d5b937caa4e4cef76e37d7218ee3085a7210941
Azhes <graystonea@gmail.com>
2021-04-06 18:50:27 -0700

    improve dark mode button and add css and JS (not working yet)

--- JS/app.js
+++ JS/app.js
@@ -2,6 +2,12 @@
     - select dark mode button class
     - use toggle method to switch between dark mode and light mode themes
 */
+const checkbox = document.getElementById('checkbox');
+
+checkbox.addEventListener('change', () => {
+    // change theme
+    document.indexbody.classList.toggle('light');
+})
 
 /* Function to make the new note button create note taking area 
     - select new note button class
